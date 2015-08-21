e4d4b3e8fbdbe31f17d0ce208183239b9df44e43
550843110@qq.com <550843110@qq.com>
2015-08-21 09:52:59 +0800

    更新说明文件

---
+++ src/js/f7.init.js
@@ -0,0 +1,14 @@
+/**
+ * Created by AnyGong on 2015/8/20.
+ */
+//“≥√Ê≥ı ºªØ
+var app = new Framework7({
+    modalTitle: 'Framework7',
+    animateNavBackIcon: true,
+    ajaxLinks: 'a.ajax'
+});
+var $$ = Dom7;
+
+var mainView = app.addView('.view-main', {
+    dynamicNavbar: true
+});
